fe1655e16a3311d47b8003ed66194100c7dd0689
Avi Menon <avimenon@Avis-MacBook-Pro.local>
2018-10-25 20:57:36 -0400

    removed cmd and web from master

--- cmd/parse_url.py
+++
@@ -1,9 +0,0 @@
-def get_lyrics(url):
-    request = urllib2.Request(url)
-    request.add_header("Authorization", "Bearer " + "your-client-access-token")
-    request.add_header("User-Agent",
-                       "curl/7.9.8 (i686-pc-linux-gnu) libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)")
-    page = urllib2.urlopen(request)
-    soup = BeautifulSoup(page, "lxml")
-    lyrics = soup.find("div", class_= "lyrics")
-    return lyrics.text
